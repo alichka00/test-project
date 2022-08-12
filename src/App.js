776e706f8534bc8cbf526b6f608b485d0fc5b94f
alichka00 <101561291+alichka00@users.noreply.github.com>
2022-08-12 12:34:01 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./store/todoSlice";
+import { TodoList } from "./components/TodoList";
+import { InputField } from "./components/InputField";
+import "./App.css";
+
+export function App() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
+  };
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      <TodoList />
+    </div>
+  );
+}
